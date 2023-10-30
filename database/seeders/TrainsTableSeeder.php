e5a5549e15ef228e929dd20dc01749d20fac9068
lorenzo-neri <lorenzo21neri@gmail.com>
2023-10-30 23:56:32 +0100

    add seeder

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -21,9 +21,13 @@ class TrainsTableSeeder extends Seeder
             $train = new Train();
             $train->company = $faker->company();
             $train->departure_station = $faker->city();
-            $train->departure_time = $faker->dateTimeThisMonth('+10 days');
+
+            $train->departure_time = $faker->dateTimeInInterval('now', '+10 days');
+
             $train->arrival_station = $faker->city();
-            $train->arrival_time = $faker->dateTimeBetween($train->departure_time, $train->departure_time . '+2 days');
+
+            $train->arrival_time = $faker->dateTimeInInterval($train->departure_time, '+3 days');
+
             $train->train_code = $faker->numberBetween(0, 200);
             $train->carriages = $faker->randomDigitNotNull();
             $train->delay = $faker->boolean();
