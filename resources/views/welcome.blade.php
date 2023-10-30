037d69ea3ce47547f42587aeebd0893e0a2d7bc9
lorenzo-neri <lorenzo21neri@gmail.com>
2023-10-31 00:25:30 +0100

    add elements in page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,29 @@
 
     
     <h1>{{$message}}</h1>
-    
-    @foreach($trains as $key => $train)
-    <div>{{$train['company']}}</div>
-    @endforeach
+
+    <div class="row">
+
+        @foreach($trains as $key => $train)
+        <div class="col p-2">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">{{$train['company']}}</h5>
+                <h6 class="card-subtitle mb-2 text-muted ">Stazione di partenza: {{$train['departure_station']}}</h6>
+                <p class="card-text">Partenza: {{$train['departure_time']}}</p>
+                <p class="card-text">Stazione d'arrivo: {{$train['arrival_station']}}</p>
+                <p class="card-text">Arrivo: {{$train['arrival_time']}}</p>
+                <p class="card-text">Codice Treno: {{$train['train_code']}}</p>
+                <p class="card-text">Carrozze: {{$train['carriages']}}</p>
+                <p class="card-text">In ritardo: {{$train['delay'] ? 'yes' : 'no'}}</p>
+                <p class="card-text">Cancellato: {{$train['canceled'] ? 'yes' : 'no'}}</p>
+                
+              </div>
+            </div>
+            
+        </div>
+        @endforeach
+    </div>
     
 </div>
 @endsection
